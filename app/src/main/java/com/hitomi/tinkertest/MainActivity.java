ffabad401a36bfa6da3428e913002c7e2ed0d1d2
Hitomis <196425254@qq.com>
2017-05-23 16:02:25 +0800

    init

--- app/src/main/java/com/hitomi/tinkertest/MainActivity.java
+++ app/src/main/java/com/hitomi/tinkertest/MainActivity.java
@@ -1,13 +1,38 @@
 package com.hitomi.tinkertest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Tinker.MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.e(TAG, "i am on patch onCreate");
+
+        findViewById(R.id.btn_tinker).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TinkerInstaller.onReceiveUpgradePatch(
+                        getApplicationContext(), Environment.getExternalStorageDirectory().getAbsolutePath() + "/patch_signed_7zip.apk");
+            }
+        });
+
+        findViewById(R.id.btn_test).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Test tinker!!!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
